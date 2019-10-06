f6e7e01e583c6e3378b70e77a692bfabaa7f8f3a
marceloarauj <42809145+marceloarauj@users.noreply.github.com>
2019-10-06 14:10:02 -0300

    Preenchimento da base

--- Main.py
+++ Main.py
@@ -1,14 +1,14 @@
 import Modelos_Treinados
 from sklearn.feature_extraction.text import CountVectorizer
   
-
-
 def classificador_de_linguagem():
     svm = Modelos_Treinados.random_forest_treinado()
-    codigo = ['using']
+    codigo = ["function ABC(){}"]
     codigo = svm['vetor'].transform(codigo)
 
     print(svm['modelo'].predict(codigo))
 
 def classificador_de_padrao(linguagem):
-    
+    print('Em desenvolvimento')
+
+classificador_de_linguagem()
